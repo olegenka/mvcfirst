abbb6a0b48c440923f83db715b72cae806df6130
kill Bill <60369904+olegenka@users.noreply.github.com>
2022-05-12 15:03:13 +0300

    Initial commit

---
+++ src/main/java/com/example/mvcfirst/MvcfirstApplication.java
@@ -0,0 +1,14 @@
+package com.example.mvcfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcfirstApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MvcfirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mvcfirst/MvcfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcfirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
